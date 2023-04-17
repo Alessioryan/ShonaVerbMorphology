5becb924c5bb9cab2b59276a7903620d1e881898
Tosolini <tosolini@uw.edu>
2023-04-16 21:51:54 -0700

    Add default_subject_prefixes

---
+++ subject_prefixes.py
@@ -0,0 +1,31 @@
+
+# DEFAULT TO MANYIKA VARIETY
+
+# Subject Prefixes
+
+default_subject_prefixes = {
+    "1sg": "ndi",
+    "1pl": "ti",
+    "2sg": "u",
+    "2pl": "mu",
+    "cl1": "a", # OR u
+    "cl2": "Va",
+    "cl3": "u",
+    "cl4": "i",
+    "cl5": "ri",
+    "cl6": "a",
+    "cl7": "ci",
+    "cl8": "Zi",
+    "cl9": "i",
+    "cl10": "dzi",
+    "cl11": "ru", # OR rwu
+    "cl12": "tu", # OR twu
+    "cl13": "ka",
+    "cl14": "hu", # OR hwu
+    "cl15": "ku",
+    "cl16": "pa",
+    "cl17": "ku",
+    "cl18": "mu",
+    "cl19": "Si",
+    "cl21": "ri"
+}
